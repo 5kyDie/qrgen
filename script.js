d6babc362af990dfe61dd270cf24ef81f952f78a
5kyDie <101502038+5kyDie@users.noreply.github.com>
2023-01-23 07:03:38 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value;
+    if(!qrValue) return; // if input is empy then return from here
+    generateBtn.innerText = "Generating QR Code...";
+    // gettin a QR code of user entered value using the qserver
+    // api and passing the api returned img src to qrImg
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => { // once QR Code img loaded
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value) {
+        wrapper.classList.remove("active");
+    }
+});
